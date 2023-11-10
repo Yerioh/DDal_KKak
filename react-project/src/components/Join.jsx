94d379c4e23aae28d555d7892987213ed87b2975
iimmuunnee <glgmsl@naver.com>
2023-11-10 14:24:13 +0900

    231110 join.jsx, user.js 아이디 중복체크 기능 추가 -임휘훈-

--- react-project/src/components/Join.jsx
+++ react-project/src/components/Join.jsx
@@ -9,8 +9,9 @@ const Join = () => {
   const idRef = useRef(); // 아이디
   const pwRef = useRef(); // 비밀번호
   const pw2Ref = useRef(); // 비밀번호 확인
-  const nameRef = useRef() // 이름
-  const numberRef = useRef() // 휴대전화
+  const nameRef = useRef(); // 이름
+  const emailRef = useRef(); // 이메일
+  const numberRef = useRef(); // 휴대전화
   const spanRef = useRef(); // 중복체크 text
   const userAdd = useRef(); // 주소
   const postNum = useRef(); // 우편번호
@@ -25,41 +26,61 @@ const Join = () => {
   /** 모달 보여지게 하는 함수 */
   const handleShow = () => setShow(true);
   const [text, setText] = useState("");
-  
-  // 2023-11-09 17:36 임휘훈 작성
+
+  // 2023-11-10 12:31 임휘훈 작성
   const join_btn = () => {
     console.log("회원가입 axios 작성하기");
+    if (pwRef.current.value == pw2Ref.current.value) {
+      axios.post("/user/join", {
+        userId: idRef.current.value, // 아이디
+        userPw: pwRef.current.value, // 비밀번호
+        checkPw: pw2Ref.current.value, // 비밀번호 확인
+        userName: nameRef.current.value, // 이름
+        useremail: emailRef.current.value, // 이메일
+        phone: numberRef.current.value, // 전화번호
+        postNumber: postNum.current.value, // 우편번호
+        doro: userAdd.current.value, // 도로명주소
+        detailAddress: addDetail.current.value, // 상세주소
+      });
+    }
   };
   // 임휘훈 작성 끝
 
-  //중복체크 함수
-  // const checkId = () => {
-  //   console.log("사용자 입력 아이디", idRef.current.value);
+  /**중복체크 함수*/
+  const checkId = () => {
+    console.log("사용자 입력 아이디", idRef.current.value);
+    // 데이터 전송
+    if (idRef.current.value !== "") {
+      console.log("아이디 뭐임?", idRef.current.value);
+      axios
+        .post("/user/checkId", {
+          id: idRef.current.value,
+        })
+        .then((res) => {
+          console.log("중복체크 결과", res.data);
 
-  //   if (idRef.current.value !== "") {
-  //     // 데이터 전송
-  //     axios
-  //       .post("/user/checkId", {
-  //         id: idRef.current.value,
-  //       })
-  //       .then((res) => {
-  //         console.log("중복체크 결과", res.data.result);
+          if (res.data === true) {
+            idRef.current.disabled = true;
+            spanRef.current.style = "color:gray";
+            setText("※ 사용 가능한 아이디 입니다.");
+          } else {
+            idRef.current.value = "";
+            idRef.current.focus();
+            spanRef.current.style = "color:red";
+            setText(
+              "※ 사용 불가능한 아이디 입니다. 다른 아이디를 입력해주세요."
+            );
+          }
+        });
+    } else {
+      // 아이디 칸이 빈칸일때 231110 14:10 추가 -임휘훈-
+      setText("※ 아이디를 입력해주세요.");
+      idRef.current.focus();
+      spanRef.current.style = "color:red";
+    }
+  };
+  // 아이디 중복체크 함수 끝
 
-  //         if (res.data.result === "uniq") {
-  //           idRef.current.disabled = true;
-  //           spanRef.current.style = "color:gray";
-  //           setText("※ 사용 가능한 아이디 입니다.");
-  //         } else {
-  //           idRef.current.value = "";
-  //           idRef.current.focus();
-  //           spanRef.current.style = "color:red";
-  //           setText(
-  //             "※ 사용 불가능한 아이디 입니다. 다른 아이디를 입력해주세요."
-  //           );
-  //         }
-  //       });
-  //   }
-  // };
   const [address, setAddress] = useState(""); // 주소
   const [addressDetail, setAddressDetail] = useState(""); // 상세주소
 
@@ -72,7 +93,6 @@ const Join = () => {
     height: "400px",
     padding: "7px",
   };
-
 
   const onChangeOpenPost = () => {
     setIsOpenPost(true);
@@ -127,7 +147,7 @@ const Join = () => {
                   <Button
                     style={{ width: "100%" }}
                     variant="light"
-                    // onClick={checkId}
+                    onClick={checkId}
                   >
                     중복체크
                   </Button>
@@ -135,106 +155,130 @@ const Join = () => {
               </Row>
             </Form.Group>
 
-          <div className="d-grid gap mb-3">
-            <span ref={spanRef}> {text}</span>
-          </div>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>* 이메일</Form.Label>
-            <Form.Control type="text" placeholder="Enter Email Address" required />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword1">
-            <Form.Label>* 비밀번호</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Enter Password"
-              ref={pwRef}
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword2">
-            <Form.Label>* 비밀번호 확인</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Confirm Password"
-              ref={pw2Ref}
-              required
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicName">
-            <Form.Label>* 이름</Form.Label>
-            <Form.Control type="text" placeholder="Enter Name" required/>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicNum">
-            <Form.Label>* 휴대전화</Form.Label>
-            <Form.Control type="text" placeholder="Enter Phone Number" required />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicAdressNum">
-            <Row>
-              <Form.Label onClick={(e) => e.preventDefault()}>주소</Form.Label>
-              <Col>
-                <Modal show={show} onHide={handleClose}>
-                  <Modal.Header closeButton>
-                    <Modal.Title>주소</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>
-                    {isOpenPost ? (
-                      <div>
-                        <DaumPostcode
-                          style={postCodeStyle}
-                          autoClose
-                          onComplete={onCompletePost}
-                          key="postcode"
+            <div className="d-grid gap mb-3">
+              <span ref={spanRef}> {text}</span>
+            </div>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>* 이메일</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Email Address"
+                ref={emailRef}
+                required
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword1">
+              <Form.Label>* 비밀번호</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter Password"
+                ref={pwRef}
+                required
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword2">
+              <Form.Label>* 비밀번호 확인</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm Password"
+                ref={pw2Ref}
+                required
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>* 이름</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Name"
+                required
+                ref={nameRef}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicNum">
+              <Form.Label>* 휴대전화</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Phone Number"
+                required
+                ref={numberRef}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicAdressNum">
+              <Row>
+                <Form.Label onClick={(e) => e.preventDefault()}>
+                  주소
+                </Form.Label>
+                <Col>
+                  <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>주소</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      {isOpenPost ? (
+                        <div>
+                          <DaumPostcode
+                            style={postCodeStyle}
+                            autoClose
+                            onComplete={onCompletePost}
+                            key="postcode"
                           />
-                      </div>
-                    ) : null}
-                  </Modal.Body>
-                  <Modal.Footer>
-                    <Button
-                      className="Button"
-                      variant="secondary"
-                      onClick={handleClose}
+                        </div>
+                      ) : null}
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button
+                        className="Button"
+                        variant="secondary"
+                        onClick={handleClose}
                       >
-                      닫기
-                    </Button>
-                  </Modal.Footer>
-                </Modal>
-                <Form.Control
-                  type="text"
-                  placeholder="우편번호"
-                  ref={postNum}
-                  onClick={() => {
-                    handleShow();
-                    onChangeOpenPost();
-                  }}
-                />{" "}
-              </Col>
-              <Col md={9}>
-                <Form.Control
-                  type="text"
-                  placeholder="주소"
-                  ref={userAdd}
-                  onClick={() => {
-                    handleShow();
-                    onChangeOpenPost();
-                  }}
+                        닫기
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                  <Form.Control
+                    type="text"
+                    placeholder="우편번호"
+                    ref={postNum}
+                    onClick={() => {
+                      handleShow();
+                      onChangeOpenPost();
+                    }}
+                  />{" "}
+                </Col>
+                <Col md={9}>
+                  <Form.Control
+                    type="text"
+                    placeholder="주소"
+                    ref={userAdd}
+                    onClick={() => {
+                      handleShow();
+                      onChangeOpenPost();
+                    }}
                   />
-              </Col>
-            </Row>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicAdress"></Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicAdressDetail">
-            <Form.Control type="text" placeholder="상세주소" ref={addDetail} />
-          </Form.Group>
-          <div className="d-grid gap mb-3">
-            <Button
-              className="Button-search"
-              variant="outline-info"
-              type="submit"
-              onClick={join_btn}
+                </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicAdress"
+            ></Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicAdressDetail">
+              <Form.Control
+                type="text"
+                placeholder="상세주소"
+                ref={addDetail}
+              />
+            </Form.Group>
+            <div className="d-grid gap mb-3">
+              <Button
+                className="Button-search"
+                variant="outline-info"
+                type="submit"
+                onClick={join_btn}
               >
-              회원가입
-            </Button>
-          </div>
+                회원가입
+              </Button>
+            </div>
           </Form>
         </div>
       </Container>
--- routes/user.js
+++ routes/user.js
@@ -1,78 +1,101 @@
-// user 라우터 
-// 2023-11-08 오전 09:45 임휘훈 작성
+// user 라우터
+// 2023-11-10 13:00 임휘훈 작성
 
-const express = require('express')
-const router = express.Router()
-const axios = require('axios')
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
 
-// // DB 연결
-// const db = require("../config/database")
-// let conn = db.init()
+// DB 연결
+const db = require("../config/database");
+let conn = db.init();
 
 // 회원가입 라우터
-router.post("/join", (req, res)=> {
-    let id = req.body.userId // 사용자가 입력한 ID, name 속성
-    let email = req.body.useremail // 사용자가 입력한 email
-    let pw = req.body.userPw // 사용자가 입력한 PW
-    let checkPw = req.body.checkPw // 비밀번호 확인
-    let userName = req.body.userName
-    let phone = req.body.phone // 사용자 휴대전화
-    let postNumber = req.body.postNumber // 우편번호
-    let doro = req.body.doro // 도로명 주소
-    let detailAddress = req.body.detailAddress // 상세주소
-    // 회원가입 쿼리문
-    let joinQuery = 'INSERT INTO TB_USER (USER_ID, USER_EMAIL, USER_PW, USER_NAME, USER_PHONE, USER_POSTNUMBER, USER_ADDRESS1, USER_ADDRESS2) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+router.post("/join", (req, res) => {
+  let id = req.body.userId; // 사용자가 입력한 ID, name 속성
+  let email = req.body.useremail; // 사용자가 입력한 email
+  let pw = req.body.userPw; // 사용자가 입력한 PW
+  let checkPw = req.body.checkPw; // 비밀번호 확인
+  let userName = req.body.userName; // 사용자 이름
+  let phone = req.body.phone; // 사용자 휴대전화
+  let postNumber = req.body.postNumber; // 우편번호
+  let doro = req.body.doro; // 도로명 주소 add1
+  let detailAddress = req.body.detailAddress; // 상세주소 add2
+  // 회원가입 쿼리문
+  let joinQuery =
+    'INSERT INTO TB_USER (MEMBER_ID, MEMBER_PW, MEMBER_NAME, MEMBER_EMAIL, MEMBER_PHONE, MEMBER_POST, MEMBER_ADDR1, MEMBER_ADDR2, MEMBER_LOGIN_TYPE, JOINED_AT) VALUES (?, ?, ?, ?, ?, ?, ?, ?, "M", NOW())';
 
-    // 비밀번호 확인 조건문
-    if(pw == checkPw){
-        // DB 연결
-        conn.connect()
-        conn.query(joinQuery,[id, email, pw, userName, phone, postNumber, doro, detailAddress], (err, result) => {
-            if(err){
-                console.log("ID 중복");
-            }
-            else{
-                console.log("회원가입 성공");
-            }
-        })
+  // 비밀번호 확인 조건문
+  if (pw == checkPw) {
+    // DB 연결
+    conn.connect();
+    conn.query(
+      joinQuery,
+      [id, pw, userName, email, phone, postNumber, doro, detailAddress],
+      (err, result) => {
+        if (err) {
+          console.log("ID 중복");
+        } else {
+          console.log("회원가입 성공");
+        }
+      }
+    );
+  } else {
+    console.log("비밀번호 확인 오류");
+  }
+});
+
+// 아이디 중복 체크
+router.post("/checkId", (req, res) => {
+  let id = req.body.id;
+  let query = "SELECT MEMBER_ID FROM TB_MEMBER WHERE MEMBER_ID = ?";
+  console.log("아이디 머임", id);
+  conn.connect();
+  conn.query(query, [id], (err, result) => {
+    if (err) {
+      console.log("아이디 중복체크 쿼리문 에러");
+    } else {
+      if (result.length == 0) {
+        // 아이디 조회결과 0개
+        console.log("회원가입 가능");
+        res.send(JSON.stringify(true));
+      } else {
+        console.log("이미 존재하는 ID");
+        res.send(JSON.stringify(false));
+      }
     }
-    else{
-        console.log("비밀번호 확인 오류");
-    }
-})
+  });
+});
 
 // 로그인 라우터
-router.post('/login', (req, res)=> {
-    let id = req.body.userId; // 입력한 아이디
-    let pw = req.body.userPw; // 입력한 비밀번호
-    // 로그인 쿼리문
-    let loginQuery = 'SELECT USER_ID, USER_PW, USER_NAME FROM TB_USER WHERE USER_ID = ?'
-    // DB 연결
-    conn.connect()
-    conn.query(loginQuery, [id], (err, result) => {
-        if(err){
-            console.log('로그인 쿼리문 에러');
-        }
-        else{
-            // 아무것도 조회 되지 않으면
-            if(result.length == 0){
-                console.log('아이디가 존재하지 않습니다.');
-            }
-            else{
-                if(result[0].USER_ID == id && result[0].USER_PW == pw){
-                    console.log(result[0].USER_ID, "님 로그인 성공");
+router.post("/login", (req, res) => {
+  let id = req.body.userId; // 입력한 아이디
+  let pw = req.body.userPw; // 입력한 비밀번호
+  // 로그인 쿼리문
+  let loginQuery =
+    "SELECT USER_ID, USER_PW, USER_NAME FROM TB_USER WHERE USER_ID = ?";
+  // DB 연결
+  conn.connect();
+  conn.query(loginQuery, [id], (err, result) => {
+    if (err) {
+      console.log("로그인 쿼리문 에러");
+    } else {
+      // 아무것도 조회 되지 않으면
+      if (result.length == 0) {
+        console.log("아이디가 존재하지 않습니다.");
+      } else {
+        if (result[0].USER_ID == id && result[0].USER_PW == pw) {
+          console.log(result[0].USER_ID, "님 로그인 성공");
 
-                    // 로그인 성공 후 사용자 세션 생성
-                    let userName = result[0].USER_NAME
-                    let seName = req.session.name = userName
-                    let seLogin = req.session.login = true
-                }
-                else{
-                    console.log("로그인 실패");
-                }
-            }
+          // 로그인 성공 후 사용자 세션 생성
+          let userName = result[0].USER_NAME;
+          let seName = (req.session.name = userName);
+          let seLogin = (req.session.login = true);
+        } else {
+          console.log("로그인 실패");
         }
-    })
-})
+      }
+    }
+  });
+});
 
 module.exports = router;
