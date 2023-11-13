dafa0036c22f1e01c29b45e6059279630c48efde
KimHyeoungGyun <138184795+KimHyeoungGyun@users.noreply.github.com>
2023-11-13 16:06:26 +0900

    23.11.13 임시 헤더, 마이페이지 push
    마이페이지(개인정보수정 페이지), 로그인 헤더, 로그아웃시 헤더

---
+++ react-project/src/components/SaveImage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/SaveImage.css";
+
+const SaveImage = () => {
+  return (
+    <div className="saveImage">
+      <h1>내 저장 이미지</h1>
+    </div>
+  );
+};
+
+export default SaveImage;
