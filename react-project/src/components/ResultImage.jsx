3e4c9c90a1f897a54f106a0f71b071e8a717a0b6
나범수 <123361606+pass98@users.noreply.github.com>
2023-11-21 11:11:44 +0900

    1121 이미지출력사이트 레이아웃 수정 나범수
    이미지 출력사이트 수정했습니다.
    1. 네거티브 프롬포트 제거
    2. 마진값 수정
    3. 스타일 많이 수정

--- react-project/src/components/ResultImage.jsx
+++ react-project/src/components/ResultImage.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import "../css/ImageResult.css";
-import Button from 'react-bootstrap/Button'
 import axios from '../axios'
 import { useNavigate } from "react-router-dom";
 
@@ -13,22 +12,23 @@ const ResultImage = () => {
   // 23-11-16 오후 14:30 박지훈 작성
   // S3 버킷 기본 주소 값
   const s3Url = process.env.REACT_APP_AWS_BUCKET_URL  
-  // const imgData = location.state.imgData
-  const imgData = ['108488627855757649516/b6bef82a-48ec-5f41-c240-31e3c174e544.png','108488627855757649516/c168fb51-89d2-5676-d90b-04eb401fa572.png',
-'108488627855757649516/c168fb51-89d2-5676-d90b-04eb401fa572.png', '108488627855757649516/c168fb51-89d2-5676-d90b-04eb401fa572.png','108488627855757649516/c168fb51-89d2-5676-d90b-04eb401fa572.png']
+  const imgData = location.state.imgData
+//   const imgData = ['108488627855757649516/b6bef82a-48ec-5f41-c240-31e3c174e544.png','108488627855757649516/c168fb51-89d2-5676-d90b-04eb401fa572.png',
+// '108488627855757649516/c168fb51-89d2-5676-d90b-04eb401fa572.png', '114662496405123443827/6c77b248-6eb1-6588-67b7-558e0acf4f0f.png','114662496405123443827/6c77b248-6eb1-6588-67b7-558e0acf4f0f.png']
+ 
 
  //이미지를 선택하면 그림자 값 유지
  const [selectedImage, setSelectedImage] = useState(null);
 
   // 선택한 이미지
   const [imgClick, setImgClick] = useState([])
-  // const positive = location.state.positivePrompt
+  const positive = location.state.positivePrompt
   // const negative = location.state.negativePrompt
-  const positive = 'test'
-  const negative = 'test'
+  // const positive = 'test'
+  // const negative = 'test'
 
-  // const countImg = location.state?.countImg || 1;
-  const countImg = 5
+  const countImg = location.state?.countImg || 1;
+
   const containerClass = 
     countImg === 4 ? "four-images" : countImg === 5 ? "five-images" : "";
   // 출력 이미지가 4개인 경우와 5개인 경우의 개별 레이아웃 생성
@@ -77,9 +77,9 @@ const ResultImage = () => {
     <div className={`result-body `}>
       <div className="result-containor">
         <div className="input-area">
-          <div className="keywrod-area">
+          <div className="keyword-area">
           {/* <div id="keywordbox"></div> */}
-            {positive}
+            <span className="keyword-label">keyword</span><span className="keyword-content">{positive}</span>
           </div>
             {/* <div id="excptbox"></div> */}
           </div>
