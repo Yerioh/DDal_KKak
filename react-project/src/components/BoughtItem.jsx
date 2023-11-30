57201ee8acd55f9d5c58d4d2a9d03bac5ceca554
조성민 <jsm9002@naver.com>
2023-11-30 14:00:03 +0900

    231130 13:59 리뷰쓰기모달추가 -조성민

--- react-project/src/components/BoughtItem.jsx
+++ react-project/src/components/BoughtItem.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReviewWrite from './ReviewWrite.jsx'
 import '../css/BoughtItem.css'
 
 
 const BoughtItem = ({order_at, order_de_img, prod_name, order_prod_info, order_de_cnt, order_detail_price}) => {
+    const [isOpen, setIsOpen] = useState(false); // 모달 상태 State
+    const [deliverState, setDeliverState] = useState(true)
 
+
+    /**모달 열고 닫는 상태 함수*/
+    const openModalHandler = (index) => {
+        // isOpen의 상태를 변경하는 메소드를 구현
+        // !false -> !true -> !false
+        setIsOpen((prevIsOpen) => !prevIsOpen);
+    };
   return (
     <div className='BoughtItem item-first-container'>
             <div className='item-second-container'>
@@ -36,9 +46,30 @@ const BoughtItem = ({order_at, order_de_img, prod_name, order_prod_info, order_d
                     .toString()
                     .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}원</div>
                 </div>
-                <div className='inner-forth-box' >
-                    물품제작중
-                </div>
+                {deliverState == true ?
+                    <div className='inner-forth-box' style={{flexWrap:"wrap"}}>
+                        <div style={{width:"100%",height:"50%"}}>
+                        배송완료
+                        
+                        </div>
+                        <div  style={{width:"100%",height:"50%"}} >
+                        <button className='SM-btn' style={{width:"90px"}} onClick={() => { openModalHandler(); }}>
+                            리뷰쓰기
+                            {isOpen && (
+                                <ReviewWrite
+                                    isOpen={isOpen}
+                                    openModalHandler={openModalHandler}
+                                />
+                            )}
+                        </button>
+                        </div>
+                    </div>
+
+                    :
+                    <div className='inner-forth-box' >
+                        물품제작중
+                    </div>
+                }
             </div>
         </div>
   )
---
+++ react-project/src/components/ReviewWrite.jsx
@@ -0,0 +1,187 @@
+import React, { useEffect, useRef, useState } from "react";
+import "../css/ReviewWrite.css";
+import Rating from '@mui/material/Rating';
+
+
+
+
+const ReviewWrite = ({
+  isOpen, // 이미지 상세 모달 state
+  openModalHandler, // 이미지 상세 모달 함수
+}) => {
+  const [rating, setRating] = useState(5);
+  const [ratingSpeech, setRatingSpeech] = useState('')
+  const textRef = useRef('')
+  let reviewData = [];
+
+
+
+
+  const submitTest = () => {
+
+    const uploadImage = document.getElementById("file").files;
+    reviewData = {
+      'uploadImage': `${uploadImage[0].name}`,
+      'rating': `${rating}`,
+      'reviewText': `${textRef.current.value}`
+    }
+    console.log(reviewData)
+
+  }
+  const speech = (newValue) => {
+    if (newValue === null) {
+      setRating('1')
+      setRatingSpeech('별로에요')
+    } else if (newValue === 1) {
+      setRatingSpeech('별로에요')
+    } else if (newValue === 2) {
+      setRatingSpeech('보통이에요')
+    } else if (newValue === 3) {
+      setRatingSpeech('쓸만해요')
+    } else if (newValue === 4) {
+      setRatingSpeech('좋아요')
+    } else if (newValue === 5) {
+      setRatingSpeech('훌륭해요')
+    }
+  }
+
+  useEffect(() => {
+    speech(rating)
+  }, [])
+
+  return (
+    <div className="ReviewWrite">
+      
+      {isOpen ? (
+        <div className="modal-backdrop">
+          {/* 모달창 전체 크기 div */}
+          <div className="modal-container"
+            onClick={(e) => e.stopPropagation()}
+          >
+            {/* 모달창 제목 */}
+            <div className="modal-title"
+            >
+              리뷰작성
+            </div>
+            {/* 모달창 - 간단하게 지금리뷰쓰러 들어온 제품정보 */}
+            <div className="modal-goods-info"
+              style={{
+                width: "100%",
+                height: "18%",
+                // backgroundColor: "lightgoldenrodyellow",
+                display: "flex",
+                fontSize: "20px",
+                fontWeight: "bold"
+              }}
+            >
+              <div className="modal-goods-info-1">
+                <div className="modal-goods-info-2" style={{ widows: "100%" }}>
+                  <img className="modal-goods-info-img"
+                    style={{
+                      widows: "100%"
+                    }}
+                    src="./images/GM2_누끼.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div style={{ height: "100%", width: "82%" }}>
+                <div className="modal-goods-info-3" style={{ marginTop: "20px" }}>
+                  맨투맨
+                </div>
+                <div className="modal-goods-info-3">
+                  XL / 네이비
+                </div>
+              </div>
+            </div>
+            <hr style={{ backgroundColor: "lightgray" }} />
+            {/* 상품만족도(별점 주기) */}
+            <div
+              style={{
+                width: "100%",
+                height: "15%",
+              }}
+            >
+              <div style={{ fontSize: "20px" }}>받으신 상품은 어땠나요?</div>
+              <Rating className="rating-star" name="simple-controlled" defaultValue={5.0} precision={0.5} value={rating}
+                onChange={(event, newValue) => (
+                  // 별점업데이트
+                  setRating(newValue),
+                  // 별점메세지
+                  speech(newValue))}
+              />
+              <div style={{ fontSize: "20px" }}>{ratingSpeech}</div>
+            </div>
+            <hr style={{ backgroundColor: "lightgray" }} />
+            <div
+              style={{
+                width: "100%",
+                height: "25%",
+                overflowBlock: "scroll",
+              }}
+            >
+              {/* 리뷰 텍스트 입력창 */}
+              <div
+                style={{
+                  height: "95%",
+                  overflowBlock: "scroll",
+                }}
+              >
+                <div style={{ height: "15%", fontSize: "20px", margin: "0px 0px 5px 0px" }}>
+                  여러분의 의견을 알려주세요.
+                </div>
+                <textarea
+                className="modal-goods-textarea"
+                  name="reviewText"
+                  id="review"
+                  cols="80"
+                  rows="5"
+                  ref={textRef}
+                />
+              </div>
+            </div>
+            {/* 이미지 업로드 */}
+            <div style={{ height: "10%" }}>
+              {/* <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+                Upload file
+                
+                <VisuallyHidd
+                enInput type="file" />
+              </Button> */}
+              <div style={{ height: "22px", fontSize: "20px", margin: "0px 0px 5px 0px" }}>
+                리뷰이미지업로드
+              </div>
+              <input type="file"
+                id="file"
+                name="file"
+                accept="image/png, image/jpeg"
+                style={{ height: "30px", width: "92%" }}
+                className="SM-btn"
+              />
+            </div>
+
+            {/* 리뷰저장 과 닫기 버튼 */}
+            <div className="modal-goods-btnArea"
+            >
+              <button className='modal-goods-save-btn SM-btn'
+                onClick={submitTest}
+              >
+                리뷰저장
+              </button>
+              <button
+                className="modal-goods-close-btn SM-btn"
+                onClick={() => {
+                  openModalHandler();
+                }}
+              >
+                닫기
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default ReviewWrite;
